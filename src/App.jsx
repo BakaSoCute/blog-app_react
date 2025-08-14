7f5f2e4e392c766373a11926a79d41672cdfdd76
Aleksandr Omelucik <bladewizard@mail.ru>
2025-08-14 10:53:10 +0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,16 @@ function App() {
   const [todosById, setTodosById] = useState({})
   const [isTodosLoading, setTodosLoading] = useState(false)
   const [isError , setError] = useState(false)
+  const [isErrorTitle , setErrorTitle] = useState(false)
+  const [isErrorBody , setErrorBody] = useState(false)
+  const [isErrorBodyL , setErrorBodyL] = useState(false)
   const [postTitle , setPostTitle] = useState("")
   const [postBody , setPostBody] = useState("")
 
   useEffect(() => {
     setError(false)
+    setErrorTitle(false)
+    setErrorBody(false)
     setTodosLoading(true)
 
     getTodos()
@@ -42,16 +47,30 @@ function App() {
           title: postTitle,
           body: postBody
         }
+        if (todo.title.length >= 100 ) {
+          setError(true)
+          return
+        }
+        if (todo.title.length <= 4) {
+          return setErrorTitle(true)
+        }
+        if (todo.body.length >= 200) {
+          return setErrorBody(true)
+        }
+        if (todo.body.length <= 10) {
+          return setErrorBodyL(true)
+        }
         setTodosById({
           ...todosById,
           [todo.id]: todo
         })
         setTodosIds([todo.id, ...todosIds])
     
-    
+
         addTodo(todo)
   }
   function onToggleTitle (e) {
+
     setPostTitle(e.target.value)
   }
   function onToggleBody (e) {
@@ -66,6 +85,10 @@ function App() {
         onToggleTitle={onToggleTitle}
         onToggleBody={onToggleBody}
       />
+      <p>{isError && "Заголовок больше 100 символов"}</p>
+      <p>{isErrorTitle && "Заголовок меньше 4 символов"}</p>
+      <p>{isErrorBody && "Пост больше 200 символов"}</p>
+      <p>{isErrorBodyL && "Пост меньше 10 символов"}</p>
       </div>
       <div>
         <h2>Лента</h2>
